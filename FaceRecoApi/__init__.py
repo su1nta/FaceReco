098e2867228d4ef16fa6ca0ccaf466a52e10086a
su1nta <sukanta.developer@gmail.com>
2023-06-22 01:21:21 +0530

    FaceRecoApi package created

---
+++ FaceRecoApi/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__programmer__ = """Sukanta Bala"""
+__version__ = '1.0'
+
+from .api import load_image_file, detect_face, face_landmarks, face_encode, face_distance, compare_faces, mail_unknown_faces
